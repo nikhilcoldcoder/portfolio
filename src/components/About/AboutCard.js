e2e461b64586852dfd8af67009c7d12e7c9c5de1
nikhilcoldcoder <nikhilagni6@gmail.com>
2025-11-04 17:16:03 +0530

    file

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,15 +8,15 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Soumyajit Behera</span>{" "}
-            from <span className="purple">Bhubaneswar, India</span>.
+            Hi everyone! I’m <span className="purple">Nikhil Sharma </span>{" "}
+            from <span className="purple"> Jodhpur, India</span>.
             <br />
-            I’m currently working as a{" "}
+            {/* I’m currently working as a{" "}
             <span className="purple">Software Developer</span> at{" "}
-            <span className="purple">Juspay</span>.
-            <br />I hold an Integrated M.Sc. (IMSc) in{" "}
-            <span className="purple">Mathematics and Computing</span> from{" "}
-            <span className="purple">BIT Mesra</span>.
+            <span className="purple">Juspay</span>. */}
+            <br />I hold a Master’s degree in Computer Applications (MCA){" "}from{" "}
+            <span className="purple">Bikaner Technical University (BTU),</span> 
+            <span className="purple">where I developed a strong foundation in software development, programming, and emerging technologies.</span>.
             <br />
             <br />
             Outside of coding, I love engaging in activities that keep me
@@ -38,7 +38,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Nikhil</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -5,7 +5,8 @@ import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/Soumyajit_Behera.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
